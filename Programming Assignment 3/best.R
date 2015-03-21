1a21533ad338c51e21d85e82698d8e045426ab4e
iulia-stefan3 <iulia.stefan3@gmail.com>
2015-03-21 15:46:38 -0700

    reorganized R Programming files & added Getting & Cleaning Data files

--- Programming Assignment 3/best.R
+++
@@ -1,5 +0,0 @@
-# This is just a special case of the rankhospital function; source that function and evaluate it for num = "best"
-best <- function(state, outcome) {
-  source("rankhospital.R")
-  rankhospital(state, outcome, "best")
-}
